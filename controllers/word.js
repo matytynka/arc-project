ba2fa3207f082397b584e627b06bb9445d9dcc81
Wojciech Skarbek <woj.skarbek@gmail.com>
2021-01-15 15:42:47 +0100

    wordAPI update

--- controllers/word.js
+++ controllers/word.js
@@ -14,8 +14,6 @@ exports.getUnlearnedWordListHandler = async function(req, res) { return await ge
 exports.addWordHandler = async function(req, res) { await addWord(req, res); }
 exports.addWordsHandler = async function(req, res) { await addWords(req, res); }
 exports.deleteWordHandler = async function(req, res) { await deleteWord(req, res); }
-exports.learnWordUpHandler = async function(req, res) { await learnWordUp(req, res); }
-exports.learnWordDownHandler = async function(req, res) { await learnWordDown(req, res); }
 
 /**
  * Gets all the words from Firestore.
