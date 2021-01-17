edec1b8369aaeb4516e14e4cb6dbb56782d05df9
Martyna <matytynka@o2.pl>
2021-01-17 16:18:43 +0100

    openapi.yaml updated

--- api/repeatingWordsAPI.js
+++ api/repeatingWordsAPI.js
@@ -4,9 +4,10 @@ const router = express.Router();
 router.post('/', async (req, res) => {
     const words = req.body;
     let filteredWords = [];
+    let forbiddenWords = ["el", "la", "en", "del", "a", "y", "los", "las", "unos", "unas", "uno", "una", "con", "que", "de", "me", "te", "se", "nos", "os", "o"];
 
     words.forEach((word) => {
-        if (!filteredWords.includes(word.toLowerCase())) {
+        if (!filteredWords.includes(word.toLowerCase()) && !forbiddenWords.includes(word.toLowerCase())) {
             filteredWords.push(word.toLowerCase());
         }
     });
