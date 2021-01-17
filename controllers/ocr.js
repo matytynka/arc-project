7833665a7fed1aa715d92757cc385c502bf92a91
Wojciech Skarbek <woj.skarbek@gmail.com>
2021-01-17 17:27:21 +0100

    gc fixes

--- controllers/ocr.js
+++ controllers/ocr.js
@@ -3,7 +3,7 @@ const keyFilename = './configs/key.json';
 const client = new vision.ImageAnnotatorClient({keyFilename});
 
 exports.detectText = async function(path) {
-    const [result] = await client.textDetection(`gs://arc-project-bf6c3.appspot.com/${path}`);
+    const [result] = await client.textDetection(`gs://arc-project-302014.appspot.com/${path}`);
     const detections = result.textAnnotations;
     return detections[0].description.replace(/[,.]/g, "").split(" ");
 }
